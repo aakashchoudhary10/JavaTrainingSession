b1785d5734c87bfa8dc3a690d8e5d59e8f92bdff
Aakash Choudhary <aakashchoudhary@QAIT.COM>
2019-09-10 15:55:53 +0530

    Added Test1

---
+++ src/JavaPrograms/Test1.java
@@ -0,0 +1,23 @@
+package JavaPrograms;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		int a,b,c;
+		a=10;
+		b=20;
+		c=a+b;
+		
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(a++);
+		System.out.println(b++);
+		System.out.println(c++);
+		System.out.println(++a);
+		System.out.println(++b);
+		System.out.println(++c);
+
+	}
+
+}
